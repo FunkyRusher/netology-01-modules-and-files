2d7c2b7c598d137ecff5aa882f56ed154783bc96
FunkyRusher <funkyrusher@gmail.com>
2018-01-29 22:32:02 +0300

    Вынос require(‘fs’) и функции readFile на уровень модуля

--- read-all.js
+++ read-all.js
@@ -1,19 +1,19 @@
+const fs = require('fs');
+const conf = { encoding: 'utf8' };
+
+function readFile(name) {
+  return new Promise((done, fail) => {
+    fs.readFile(name, conf, (err, content) => {
+      if (err) {
+        fail(err);
+      } else {
+        done({name, content});
+      }
+    })
+  });
+}
+
 module.exports = path => {
-  const fs = require('fs');
-  const conf = { encoding: 'utf8' };
-
-  function readFile(name) {
-    return new Promise((done, fail) => {
-      fs.readFile(name, conf, (err, content) => {
-        if (err) {
-          fail(err);
-        } else {
-          done({name, content});
-        }
-      })
-    });
-  }
-
   return new Promise((done, fail) => {
     fs.readdir(path, (err, files) => {
       if (err) {
